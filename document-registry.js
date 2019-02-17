88df464db1348ab2d93835bf8952076723407d61
Svetlin Nakov <github@nakov.com>
2019-02-17 23:49:59 +0200

    fixes

--- document-registry.js
+++ document-registry.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    const documentRegistryContractAddress = '0x987ae6c8837e88dff419ac01a9a41c693ddeda33';
-    const documentRegistryContractABI = [{"constant":false,"inputs":[{"name":"hash","type":"string"}],"name":"add","outputs":[{"name":"dateAdded","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"string"}],"name":"verify","outputs":[{"name":"dateAdded","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
+    const documentRegistryContractAddress = '0x949c76215c7333c1697297fcd6307703aa77115c';
+    const documentRegistryContractABI = [{"constant":false,"inputs":[{"name":"hash","type":"string"}],"name":"add","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"hash","type":"string"}],"name":"verify","outputs":[{"name":"dateAdded","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}];
 
     $('#linkHome').click(function() { showView("viewHome") });
     $('#linkSubmitDocument').click(function() { showView("viewSubmitDocument") });
@@ -32,18 +32,24 @@ $(document).ready(function() {
         $('#errorBox>header').click(function(){ $('#errorBox').hide(); });
     }
     
-    function uploadDocument() {
+    async function uploadDocument() {
         if ($('#documentForUpload')[0].files.length == 0)
             return showError("Please select a file to upload.");
+		if (window.ethereum)
+			try {
+				await window.ethereum.enable();
+			} catch (err) {
+                return showError("Access to your Ethereum account rejected.");
+			}
         let fileReader = new FileReader();
         fileReader.onload = function() {
             let documentHash = sha256(fileReader.result);
             if (typeof web3 === 'undefined')
-                return showError("Please install MetaMask to access the Ethereum Web3 API from your Web browser.");
+                return showError("Please install MetaMask to access the Ethereum Web3 injected API from your Web browser.");
             let contract = web3.eth.contract(documentRegistryContractABI).at(documentRegistryContractAddress);
-            contract.add(documentHash, function(err, result, r1, r2, r3) {
+            contract.add(documentHash, function(err, result) {
                 if (err)
-                    return showError("Smart contract call failed: " + e);
+                    return showError("Smart contract call failed: " + err);
                 showInfo(`Document ${documentHash} <b>successfully added</b> to the registry.`);
             });            
         }
@@ -62,7 +68,7 @@ $(document).ready(function() {
             contract.verify(documentHash, function(err, result) {
                 if (err)
                     return showError("Smart contract call failed: " + e);
-                let contractPublishDate = result.c; // Take the output from the execution
+                let contractPublishDate = result.toNumber(); // Take the output from the execution
                 if (contractPublishDate > 0) {
                     let displayDate = new Date(contractPublishDate * 1000).toLocaleString();
                     showInfo(`Document ${documentHash} is <b>valid<b>, date published: ${displayDate}`);
